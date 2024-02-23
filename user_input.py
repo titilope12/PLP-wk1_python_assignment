0b25680d9e62e64a3bc1a9ecd64abfea11d350fd
adelekevictor12 <124449439+adelekevictor12@users.noreply.github.com>
2024-02-23 08:09:36 +0100

    First commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+location = input("Enter your current location : ")
+print("Hello, " + name + " you are " + age + " years old and you are from " + location)
